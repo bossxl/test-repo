3f43da11825aeee4e92229f79b552fc526bd1076
Paul Boss <pboss@codiscope.com>
2015-04-23 16:00:48 -0400

    Test

--- bad.js
+++ bad.js
@@ -1,18 +1,18 @@
 eval('jsCode'+usercontrolledVal )
 window.eval('jsCode'+usercontrolledVal )
-"new Function('jsCode'+usercontrolledVal ) ;
- new Function('arg','arg2','jsCode'+usercontrolledVal )"
-"Function('jsCode'+usercontrolledVal ) ;
- Function('arg','arg2','jsCode'+usercontrolledVal )"
-setTimeout('jsCode'+usercontrolledVal ,timeMs)
-global.setTimeout('jsCode'+usercontrolledVal ,timeMs)
-setInterval('jsCode'+usercontrolledVal ,timMs)
-global.setInterval('jsCode'+usercontrolledVal ,timMs)
-setImmediate('jsCode'+usercontrolledVal )
-global.setImmediate('jsCode'+usercontrolledVal )
-execScript('jsCode'+usercontrolledVal ,'JScript')
-crypto.generateCRMFRequest('CN=0',0,0,null,'jsCode'+usercontrolledVal,384,null,'rsa-dual-use')
-parseFromString(bar+'bar')
-foo.addEventListener(bar+'bar')
-onmessage=bar
-if(x=y);
+// "new Function('jsCode'+usercontrolledVal ) ;
+//  new Function('arg','arg2','jsCode'+usercontrolledVal )"
+// "Function('jsCode'+usercontrolledVal ) ;
+//  Function('arg','arg2','jsCode'+usercontrolledVal )"
+// setTimeout('jsCode'+usercontrolledVal ,timeMs)
+// global.setTimeout('jsCode'+usercontrolledVal ,timeMs)
+// setInterval('jsCode'+usercontrolledVal ,timMs)
+// global.setInterval('jsCode'+usercontrolledVal ,timMs)
+// setImmediate('jsCode'+usercontrolledVal )
+// global.setImmediate('jsCode'+usercontrolledVal )
+// execScript('jsCode'+usercontrolledVal ,'JScript')
+// crypto.generateCRMFRequest('CN=0',0,0,null,'jsCode'+usercontrolledVal,384,null,'rsa-dual-use')
+// parseFromString(bar+'bar')
+// foo.addEventListener(bar+'bar')
+// onmessage=bar
+// if(x=y);
